5bcee192e3e04196e68063f448759aea45139141
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-09-03 19:35:55 -0700

    Made CORENLP_CLIENT a non-static variable

--- util.py
+++ util.py
@@ -5,15 +5,14 @@ Utilities to annotate documents with Stanza
 from stanza.nlp.corenlp import CoreNLPClient, AnnotationException
 from . import settings
 
-CORENLP_CLIENT = CoreNLPClient(settings.ANNOTATOR_ENDPOINT)
-
 def annotate_document(doc_gloss):
     """
     Annotate a document using the CoreNLPClient
     :param doc_gloss Gloss of the document to parse.
     :returns A list of Sentences and Mentions
     """
-    document = CORENLP_CLIENT.annotate(doc_gloss, settings.DEFAULT_ANNOTATORS)
+    client = CoreNLPClient(settings.ANNOTATOR_ENDPOINT)
+    document = client.annotate(doc_gloss, settings.DEFAULT_ANNOTATORS)
 
     return [], []
 
